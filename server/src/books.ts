42834fcfeffc6d35dee6ad7a8f8b4b9fe6c2b09b
izmi.komar <komar.izmi@gmail.com>
2024-04-04 19:18:55 +0700

    Add books API routes and data generation

---
+++ server/src/books.ts
@@ -0,0 +1,72 @@
+import expres from "express";
+import { faker } from "@faker-js/faker";
+const router = expres.Router();
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  publishedDate: Date | string;
+  totalSales: number;
+  price: {
+    amount: number;
+    currency: string;
+  };
+};
+
+const data: Book[] = Array.from({ length: 100 }, () => ({
+  id: crypto.randomUUID(),
+  title: faker.lorem.words(),
+  author: faker.person.fullName(),
+  description: faker.lorem.paragraph(),
+  publishedDate: faker.date.past(),
+  totalSales: Math.floor(Math.random() * 1000),
+  price: {
+    amount: Math.floor(Math.random() * 100),
+    currency: "USD",
+  },
+}));
+
+router.get("/", async (req, res) => {
+  res.json(data);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const book = data.find((book) => book.id === id);
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  res.json(book);
+});
+
+router.post("/", async (req, res) => {
+  const book: Book = req.body;
+  book.id = crypto.randomUUID();
+  data.push(book);
+  res.json(book);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const book: Book = req.body;
+  const findBook = data.find((book) => book.id === id);
+  if (!findBook) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  Object.assign(findBook, book);
+  res.json(findBook);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const index = data.findIndex((book) => book.id === id);
+  if (index === -1) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  data.splice(index, 1);
+  res.json({ message: "Book deleted" });
+});
+
+export default router;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import routeBooks from "./books";
 const app = express();
 dotenv.config();
-
-const port = process.env.PORT || 3000
-console.log('port', port)
+const port = process.env.PORT || 3000;
+console.log("port", port);
 
 // create a async simple route
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
 });
 
+app.use("/books", routeBooks);
+
 // start the server
-app.listen(3000, async() => {
+app.listen(3000, async () => {
   console.log(`Server started at http://localhost:${port} 🚀`);
 });

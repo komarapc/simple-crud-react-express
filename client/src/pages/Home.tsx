201a60d76bb9e91e165f2f37a8074363c0f2bb33
izmi.komar <komar.izmi@gmail.com>
2024-04-04 20:07:14 +0700

    Add CRUD functionality and pagination to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,189 @@
 import { cn } from "@/lib/utils";
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  getKeyValue,
+  Input,
+  Pagination,
+  Select,
+  SelectItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { useEffect, useMemo, useState } from "react";
+import { Edit2, Trash2 } from "lucide-react";
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  publishedDate: Date | string;
+  totalSales: number;
+  price: {
+    amount: number;
+    currency: string;
+  };
+};
+const showPerPage = ["10", "20", "30", "50"];
+const api = "http://localhost:3000";
+const fetchBooks = async () => {
+  const response = await fetch(`${api}/books`);
+  const data = await response.json();
+  return data;
+};
 const Home = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [perPage, setPerPage] = useState<any>(new Set(["10"]));
+  const [searchTitle, setSearchTitle] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+
+  const filteredBooks = useMemo(() => {
+    const limit =
+      Number(Array.from(perPage)[0]) ||
+      Number(getKeyValue(perPage, "currentKey")) ||
+      10;
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    const query = searchTitle.toLowerCase();
+
+    const booksFiltered = books.filter((book) =>
+      book.title.toLowerCase().includes(query)
+    );
+    const booksSliced = booksFiltered.slice(start, end);
+    const totalData = booksFiltered.length;
+    const totalPage = Math.ceil(totalData / limit);
+    return { books: booksSliced, totalPage, totalData };
+  }, [books, searchTitle, perPage, page]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchBooks();
+      setBooks(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchTitle]);
+
   return (
     <>
-      <div className="p-10">
-        <Card className={cn(" bg-card text-card-foreground w-96")}>
+      <div className="container mx-auto py-10 font-inter">
+        <Card className={cn("bg-card text-card-foreground")}>
           <CardHeader>
-            <h1 className="text-lg font-semibold">Home</h1>
+            <div>
+              <h2 className="text-2xl font-semibold">Simple CRUD books </h2>
+              <p className="text-sm">
+                This is a simple CRUD application with ReactJS, Prisma, and
+                NextUI.
+              </p>
+            </div>
           </CardHeader>
           <Divider />
           <CardBody>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
+            <Table
+              aria-label="books table"
+              aria-labelledby="books table"
+              removeWrapper
+              topContent={
+                <div className="flex items-center justify-between w-full">
+                  <div className="w-full flex items-center gap-4">
+                    <Select
+                      label="Show per page"
+                      items={showPerPage.map((item) => ({
+                        label: item,
+                        value: item,
+                      }))}
+                      selectedKeys={perPage}
+                      onSelectionChange={setPerPage}
+                      className="w-full lg:w-32"
+                    >
+                      {(item) => (
+                        <SelectItem key={item.value} value={item.value}>
+                          {item.label}
+                        </SelectItem>
+                      )}
+                    </Select>
+                    <Input
+                      label="Search title"
+                      onChange={(e) => setSearchTitle(e.target.value)}
+                      value={searchTitle}
+                      className="w-full lg:w-64"
+                    />
+                  </div>
+                  <div></div>
+                </div>
+              }
+              bottomContent={
+                <div className="flex items-center justify-center">
+                  <Pagination
+                    total={filteredBooks.totalPage}
+                    onChange={setPage}
+                    radius="full"
+                    aria-label="pagination"
+                    aria-labelledby="pagination"
+                  />
+                </div>
+              }
+            >
+              <TableHeader>
+                <TableColumn className="uppercase">title</TableColumn>
+                <TableColumn className="uppercase">author</TableColumn>
+                <TableColumn className="uppercase">publised</TableColumn>
+                <TableColumn className="uppercase">sales</TableColumn>
+                <TableColumn className="uppercase">price</TableColumn>
+                <TableColumn className="uppercase">profit</TableColumn>
+                <TableColumn className="uppercase">action</TableColumn>
+              </TableHeader>
+              <TableBody>
+                {filteredBooks.books.map((book) => (
+                  <TableRow key={book.id}>
+                    <TableCell className="capitalize">{book.title}</TableCell>
+                    <TableCell>{book.author}</TableCell>
+                    <TableCell>
+                      {new Date(book.publishedDate as string).toDateString()}
+                    </TableCell>
+                    <TableCell>{book.totalSales}</TableCell>
+                    <TableCell>
+                      {book.price.amount} {book.price.currency}
+                    </TableCell>
+                    <TableCell>
+                      {new Intl.NumberFormat().format(
+                        book.totalSales * book.price.amount
+                      )}{" "}
+                      {book.price.currency}
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <Button
+                          color="warning"
+                          variant="flat"
+                          isIconOnly
+                          size="sm"
+                        >
+                          <Edit2 />
+                        </Button>
+                        <Button
+                          color="danger"
+                          variant="flat"
+                          isIconOnly
+                          size="sm"
+                        >
+                          <Trash2 />
+                        </Button>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </CardBody>
         </Card>
       </div>
